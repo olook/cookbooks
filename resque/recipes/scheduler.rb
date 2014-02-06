1d7e8702f3dafab1647dca876f0507490dc4c6e0
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-06 01:54:47 -0200

    Adding resque scheduler upstart file

---
+++ resque/recipes/scheduler.rb
@@ -0,0 +1,17 @@
+node[:deploy].each do |app, data|
+
+  template "/etc/init/#{app}_resque_scheduler.conf" do
+    owner 'root'
+    group 'root'
+    mode 0744
+    source "upstart"
+    variables({
+      :app_name => app,
+      :pidfile  => pidfile,
+    })
+  end
+
+  execute "start-resque-scheduler" do
+    command %Q{start #{app}_resque_scheduler}
+  end
+end
