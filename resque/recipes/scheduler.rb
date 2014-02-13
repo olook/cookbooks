683d1bcbe772807ef1ba5719f2f1a57b00eeeca7
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-13 14:34:13 -0200

    Adding check if is running

--- resque/recipes/scheduler.rb
+++ resque/recipes/scheduler.rb
@@ -11,6 +11,11 @@ node[:deploy].each do |app, data|
   end
 
   execute "start-resque-scheduler" do
-    command %Q{restart #{app}_resque_scheduler}
+    pid = File.read("/var/run/resque-scheduler.pid").to_s.strip rescue nil
+    if pid && `ps #{pid} | grep #{pid}`.to_s.size > 0
+      command %Q{restart #{app}_resque_scheduler}
+    else
+      command %Q{start #{app}_resque_scheduler}
+    end
   end
 end
