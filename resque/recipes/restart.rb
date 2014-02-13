510ae1a64c46f3958d5c99c40d519808645018f0
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-13 11:58:02 -0200

    fix missing app

--- resque/recipes/restart.rb
+++ resque/recipes/restart.rb
@@ -1,3 +1,5 @@
-execute "restart-resque" do
-  command %Q{service #{app}_resque restart}
+node[:deploy].each do |app, data|
+  execute "restart-resque" do
+    command %Q{service #{app}_resque restart}
+  end
 end
