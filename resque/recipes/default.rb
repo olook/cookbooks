3b34ed3a3d5c092d573f8f57d847c16f4cf25b47
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-14 13:54:30 -0200

    Clearify recipe

--- resque/recipes/default.rb
+++ resque/recipes/default.rb
@@ -4,18 +4,6 @@ node[:deploy].each do |app, data|
 
   pidfile = "/srv/www/#{app}/current/tmp/pids/#{app}_resque.pid"
 
-  template "/etc/monit/conf.d/#{app}_resque.monitrc" do
-    owner 'root'
-    group 'root'
-    mode 0644
-    source "monitrc.erb"
-    variables({
-      :app_name => app,
-      :pidfile  => pidfile,
-      #:max_mem  => "400 MB",
-    })
-  end
-
   template "/etc/init/#{app}_resque.conf" do
     owner 'root'
     group 'root'
@@ -27,17 +15,7 @@ node[:deploy].each do |app, data|
     })
   end
 
-  service "#{app}_resque" do
-    provider Chef::Provider::Service::Upstart
-    start_command %Q{service #{app}_resque start}
-    stop_command %Q{service #{app}_resque stop || true}
-    restart_command %Q{service #{app}_resque stop || true; service #{app}_resque start}
-    action [:enable, :restart]
+  execute "start-resque" do
+    command %Q{service #{app}_resque stop || true; service #{app}_resque start}
   end
-
-  execute "ensure-resque-is-setup-with-monit" do
-    command %Q{monit reload}
-    action :run
-  end
-
 end
