95d270215546990eb770839d5708852eaeccab21
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-03-18 20:17:09 -0300

    Refactor and fix resque recipes

--- resque/recipes/default.rb
+++ resque/recipes/default.rb
@@ -3,8 +3,9 @@ include_recipe "deploy::rails"
 node[:deploy].each do |app, data|
 
   pidfile = "/srv/www/#{app}/current/tmp/pids/#{app}_resque.pid"
+  upstart_name = "#{app}_resque"
 
-  template "/etc/init/#{app}_resque.conf" do
+  template "/etc/init/#{upstart_name}.conf" do
     owner 'root'
     group 'root'
     mode 0644
@@ -12,6 +13,7 @@ node[:deploy].each do |app, data|
     variables({
       :app_name => app,
       :pidfile  => pidfile,
+      :upstart_name => upstart_name
     })
   end
 
@@ -19,20 +21,21 @@ node[:deploy].each do |app, data|
     owner 'root'
     group 'root'
     mode 0644
-    source "resque-pool-monitrc.erb"
+    source "monitrc.erb"
     variables({
       :app_name => app,
       :pidfile  => pidfile,
+      :upstart_name => upstart_name
     })
   end
 
   execute "stop-resque" do
-    command %Q{service #{app}_resque stop || true}
+    command %Q{service #{upstart_name} stop || true}
     action :run
   end
 
   execute "start-resque" do
-    command %Q{service #{app}_resque start}
+    command %Q{service #{upstart_name} start}
     action :run
   end
 end
--- resque/recipes/scheduler.rb
+++ resque/recipes/scheduler.rb
@@ -1,16 +1,33 @@
 node[:deploy].each do |app, data|
 
-  template "/etc/init/#{app}_resque_scheduler.conf" do
+  pidfile = "/srv/www/#{app}/current/tmp/pids/resque_scheduler.pid"
+  upstart_name = "#{app}_resque_scheduler"
+
+  template "/etc/init/#{upstart_name}.conf" do
     owner 'root'
     group 'root'
-    mode 0744
+    mode 0644
     source "scheduler_upstart.erb"
     variables({
-      :app_name => app
+      :app_name => app,
+      :pidfile => pidfile,
+      :upstart_name => upstart_name
+    })
+  end
+
+  template "/etc/monit/conf.d/#{app}_resque_scheduler.monitrc" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    source "monitrc.erb"
+    variables({
+      :app_name => app,
+      :pidfile  => pidfile,
+      :upstart_name => upstart_name
     })
   end
 
   execute "start-resque-scheduler" do
-    command %Q{service #{app}_resque_scheduler stop || true; service #{app}_resque_scheduler start}
+    command %Q{service #{upstart_name} stop || true; service #{upstart_name} start}
   end
 end
