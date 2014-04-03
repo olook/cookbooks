351b9f9240c030ee79608119ccdeae34577ba406
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-04-03 16:16:09 -0300

    Adding default recipe

---
+++ statistics-api/recipes/default.rb
@@ -0,0 +1,55 @@
+app = node['statistics-api']
+
+apps_root = "/srv/statistics-api"
+
+remote_source = "https://github.com/olook/statistics-api/archive/#{app['ref']}.zip"
+current_path = "#{apps_root}/current"
+shared_path = "#{apps_root}/shared"
+revisions_path = "#{apps_root}/revisions"
+
+revision_path = "#{revisions_path}/#{app['ref']}"
+revision_file = "#{revision_path}.zip"
+
+[current_path, shared_path, "#{shared_path}/log", "#{shared_path}/pid", revisions_path, revision_path].each do |path|
+  directory path do
+    recursive true
+    owner "root"
+    group "root"
+    action :create
+  end
+end
+
+remote_file revision_file do
+  source remote_source
+  headers['Authentication'] = "Basic #{Base64.encode("#{app['github_user']}:#{app['github_pass']}")}"
+  owner 'deploy'
+  not_if { ::File.exists?(revision_path) }
+end
+
+execute "extract-and-install" do
+  cwd revisions_path
+  cmd <<-EOF
+    unzip #{revision_file}
+    mv statistics-api-#{app['ref']}/* #{revision_path}
+  EOF
+  not_if { ::File.exists?(revision_path) }
+end
+
+execute "link-current" do
+  cmd <<-EOF
+    ln -s #{shared_path}/log #{revision_path}/log
+    ln -s #{shared_path}/pid #{revision_path}/pid
+    ln -s #{revision_path} #{current_path}
+  EOF
+end
+
+template "/etc/init/statistics-api.conf" do
+  owner 'root'
+  group 'root'
+  mode 0644
+  source "upstart.conf.erb"
+  variables({
+    :pidfile  => pidfile,
+    :workers_count => 16
+  })
+end
