127e71c307bdc8adbacfc2c1b68db4622c57431b
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-04-03 18:22:03 -0300

    Adding deploy recipe

--- statistics-api/recipes/default.rb
+++ statistics-api/recipes/default.rb
@@ -1,47 +1,19 @@
-app = node['statistics-api']
-
 apps_root = "/srv/statistics-api"
 
-remote_source = "https://github.com/olook/statistics-api/archive/#{app['ref']}.zip"
 current_path = "#{apps_root}/current"
 shared_path = "#{apps_root}/shared"
 revisions_path = "#{apps_root}/revisions"
 pidfile = "#{current_path}/pid/server.pid"
 
-revision_path = "#{revisions_path}/#{app['ref']}"
-revision_file = "#{revision_path}.zip"
+package 'unzip'
 
-[current_path, shared_path, "#{shared_path}/log", "#{shared_path}/pid", revisions_path].each do |path|
+[shared_path, "#{shared_path}/log", "#{shared_path}/pid", revisions_path].each do |path|
   directory path do
     recursive true
     owner "root"
     group "root"
     action :create
   end
-end
-
-execute "download-source" do
-  command <<-EOF
-    curl -u "#{app['github_user']}:#{app['github_pass']}" -L -o "#{revision_file}" "#{remote_source}"
-  EOF
-  not_if { ::File.exists?(revision_file) }
-end
-
-execute "extract-and-install" do
-  cwd revisions_path
-  command <<-EOF
-    unzip #{revision_file}
-    mv statistics-api-#{app['ref']} #{revision_path}
-  EOF
-  not_if { ::File.exists?(revision_path) }
-end
-
-execute "link-current" do
-  command <<-EOF
-    ln -sf #{shared_path}/log #{revision_path}/log
-    ln -sf #{shared_path}/pid #{revision_path}/pid
-    ln -sf #{revision_path} #{current_path}
-  EOF
 end
 
 template "/etc/init/statistics-api.conf" do
@@ -57,5 +29,5 @@ end
 
 service "statistics-api" do
   supports :start => true, :stop => true, :restart => true
-  action :start
+  action :nothing
 end
---
+++ statistics-api/recipes/deploy.rb
@@ -0,0 +1,50 @@
+app = node['statistics-api']
+
+apps_root = "/srv/statistics-api"
+
+remote_source = "https://github.com/olook/statistics-api/archive/#{app['ref']}.zip"
+current_path = "#{apps_root}/current"
+shared_path = "#{apps_root}/shared"
+revisions_path = "#{apps_root}/revisions"
+
+revision_path = "#{revisions_path}/#{app['ref']}"
+revision_file = "#{revision_path}.zip"
+
+package 'unzip'
+
+execute "download-source" do
+  command <<-EOF
+    curl -u "#{app['github_user']}:#{app['github_pass']}" -L -o "#{revision_file}" "#{remote_source}"
+  EOF
+  not_if { ::File.exists?(revision_file) }
+end
+
+execute "extract-and-install" do
+  cwd revisions_path
+  command <<-EOF
+    unzip #{revision_file}
+    mv statistics-api-#{app['ref']} #{revision_path}
+  EOF
+  not_if { ::File.exists?(revision_path) }
+end
+
+execute "link-current" do
+  command <<-EOF
+    ln -sf #{shared_path}/log #{revision_path}/log
+    ln -sf #{shared_path}/pid #{revision_path}/pid
+    ln -sf #{revision_path} #{current_path}
+  EOF
+end
+
+execute "bundle install" do
+  cwd current_path
+  command "bundle install --without=test,development"
+end
+
+service "statistics-api" do
+  action :stop
+end
+
+service "statistics-api" do
+  action :start
+end
