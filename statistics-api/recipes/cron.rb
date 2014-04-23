de1a0e3475304d8a98abfe3cf0f6c6b8373956e4
Luis Daher <luisotaviodaher@gmail.com>
2014-04-23 18:04:08 -0300

    switching rake style

--- statistics-api/recipes/cron.rb
+++ statistics-api/recipes/cron.rb
@@ -1,11 +1,11 @@
 cron "run_simple_dashboard" do
   minute "*/30"
-  command "cd /srv/www/statistics_api/current && bundle exec rake map_reduce:run RACK_ENV=production"
+  command "cd /srv/www/statistics_api/current && bundle exec rake map_reduce:run\\[production\\]"
 end
 
 cron "run_periodic_dashboard" do
   minute "*/30"
-  command "cd /srv/www/statistics_api/current && bundle exec rake map_reduce:run_by_period RACK_ENV=production"
+  command "cd /srv/www/statistics_api/current && bundle exec rake map_reduce:run_by_period\\[production\\]"
 end
 
 execute "add_newline" do
