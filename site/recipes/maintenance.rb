a89e3e0b666fb64987e2f4153fa3181f2baa09ba
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-05-01 02:07:25 -0300

    Adding maintenance recipe

---
+++ site/recipes/maintenance.rb
@@ -0,0 +1,10 @@
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+  _action = deploy[:maintenance] == 'true' ? :create : :destroy
+  link "#{deploy[:deploy_to]}/current/public/system/maintenance.html" do
+    to "#{deploy[:deploy_to]}/current/public/baleiando.html"
+    if _action == :destroy
+      only_if "test -L #{deploy[:deploy_to]}/current/public/system/maintenance.html"
+    end
+  end
+end
