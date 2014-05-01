8defb62c529067efc8d5c2159ff4130825767fc1
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-05-01 02:46:02 -0300

    Fix action name

--- site/recipes/maintenance.rb
+++ site/recipes/maintenance.rb
@@ -1,10 +1,10 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
-  _action = deploy[:maintenance] == 'true' ? :create : :destroy
+  _action = deploy[:maintenance] == 'true' ? :create : :delete
   link "#{deploy[:deploy_to]}/current/public/system/maintenance.html" do
     to "#{deploy[:deploy_to]}/current/public/baleiando.html"
     action _action
-    if _action == :destroy
+    if _action == :delete
       only_if "test -L #{deploy[:deploy_to]}/current/public/system/maintenance.html"
     end
   end
