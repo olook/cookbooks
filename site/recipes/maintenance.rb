8b546e1e48e6b3a1277f19adc5b00a3bd92175b4
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-05-01 02:32:15 -0300

    Adding action

--- site/recipes/maintenance.rb
+++ site/recipes/maintenance.rb
@@ -3,6 +3,7 @@ node[:deploy].each do |application, deploy|
   _action = deploy[:maintenance] == 'true' ? :create : :destroy
   link "#{deploy[:deploy_to]}/current/public/system/maintenance.html" do
     to "#{deploy[:deploy_to]}/current/public/baleiando.html"
+    action _action
     if _action == :destroy
       only_if "test -L #{deploy[:deploy_to]}/current/public/system/maintenance.html"
     end
